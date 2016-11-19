ca60efd1971e418b33250972169eef1ee2a5f520
Fidel Castro <fc.fidel1995@gmail.com>
2016-11-19 13:18:05 -0500

    added jquery

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for,jsonify, request
 
 app = Flask(__name__)
 app.config.update(
@@ -16,7 +16,26 @@ def testPage():
 	que = "algo something lol lmfao"
 	return render_template('home.html',x=que)
 
+@app.route('/_add_numbers')
+def add_numbers():
+    a = request.args.get('a', 0, type=int)
+    b = request.args.get('b', 0, type=int)
+    return jsonify(result=a + b)
 
+@app.route('/add')
+def index():
+    return render_template('add.html')
+
+
+
+@app.route('/_say_something')
+def say_something():
+	a = request.args.get('a',type=str)
+	return jsonify(result=a)
+
+@app.route('/algo')
+def lol():
+	return render_template('algo.html')
 
 
 if __name__ == '__main__':
